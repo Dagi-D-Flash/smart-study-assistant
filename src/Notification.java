63c1930e859c3c284dd9473ddeb895857c964ea4
Dagi-D-Flash <dagimdemissew193@gmail.com>
2026-01-02 03:39:25 +0300

    Initial code generation from Class Diagram.

---
+++ src/Notification.java
@@ -0,0 +1,26 @@
+package GeniusGuide_System;
+
+
+/**
+ * @author dagim
+ * @version 1.0
+ * @created 28-Dec-2025 7:54:08 AM
+ */
+public class Notification {
+
+	private String message;
+	private int notificationId;
+	private Date sentDate;
+	public User  m_User ;
+
+	public Notification(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void sendNotification(){
+
+	}
+}//end Notification
---
+++ src/QuizAttempt.java
@@ -0,0 +1,25 @@
+package GeniusGuide_System;
+
+
+/**
+ * @author dagim
+ * @version 1.0
+ * @created 28-Dec-2025 7:54:08 AM
+ */
+public class QuizAttempt {
+
+	private Date attemptDate;
+	private int attemptId;
+	private int score;
+
+	public QuizAttempt(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public int calculateScore(){
+		return 0;
+	}
+}//end QuizAttempt
