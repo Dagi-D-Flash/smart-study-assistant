616ada70f8e3e02ab92c99cef98881ca2c9572ad
eyader-2121 <eyaderaddisbelete@gmail.com>
2026-01-04 23:20:10 +0400

    add leaderboard and Notification java files for testing

---
+++ src/Main/Leaderboard.java
@@ -0,0 +1,9 @@
+package GeniusGuide_System;
+
+public class Leaderboard {
+    private int leaderboardId;
+
+    public Leaderboard() {}
+
+    public void updateRanking(Student student) {}
+}
---
+++ src/Main/Notification.java
@@ -0,0 +1,14 @@
+package GeniusGuide_System;
+
+import java.util.Date;
+
+public class Notification {
+    private String message;
+    private int notificationId;
+    private Date sentDate;
+    public User m_User;
+
+    public Notification() {}
+
+    public void sendNotification() {}
+}
